0266b315c73f54681059976c2eb1b33ce3fa76e5
dominik.mozny <dominik.mozny@adesso.ch>
2017-12-18 11:24:24 +0100

    Implemented Biggest Interface

--- zurich/src/main/java/ch/adesso/dojo/zurich/Zurich.java
+++ zurich/src/main/java/ch/adesso/dojo/zurich/Zurich.java
@@ -1,12 +1,13 @@
 package ch.adesso.dojo.zurich;
 
+import ch.adesso.dojo.biggest.Biggest;
 import ch.adesso.dojo.german.InGermanPart;
 import ch.adesso.dojo.population.Population;
 
 /**
  * @author dominik.mozny
  */
-public class Zurich implements Population, InGermanPart {
+public class Zurich implements Population, InGermanPart, Biggest {
     public int getPopulation() {
         return 391359;
     }
